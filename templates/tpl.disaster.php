da87a03dc2eff0424bf2e8e4e11070e4e9619b8f
Joanna <jlit903@gmail.com>
2012-11-03 17:02:17 -0400

    added footer, header target html

---
+++ templates/tpl.disaster.php
@@ -0,0 +1,15 @@
+<div id="mainContent">
+	<div class="container">
+		<!-- main content start -->
+		
+		<div class="row-fluid">
+			<div class="span12">
+			
+				<h1>hello world</h1>
+				
+			</div>
+		</div>
+
+		
+	</div><!-- main content end -->
+</div>
