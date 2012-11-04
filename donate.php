5a9a62951f0186aebad2652bc9655e51e10e25d5
TEAM HACKDAP <disastg1@box778.bluehost.com>
2012-11-04 15:02:08 -0700

    snapshot

---
+++ donate.php
@@ -0,0 +1,5 @@
+<?
+require_once('_config.php');
+$page = new page();
+
+$page->render('tpl.donate.php');
---
+++ templates/tpl.donate.php
@@ -0,0 +1,7 @@
+<div id="mainContent" class="landing-page">
+	<div class="container">
+		<div class="donate-wrapper">
+<script type="text/javascript" src="http://www.crowdrise.com/widgets/project/SandyMonitoring/disasteraccountability/"></script>
+		</div>
+	</div>
+</div>
